4fa134ed40a2ec61495ffdbf13be76efdd2b5caf
Brunelleschk1 <brunelleschki@gmail.com>
2026-02-20 12:57:02 -0500

    cambios finales domingo 8 de febrero. No sé cuáles son los K normales, no sé cómo arreglar la divergencia Tampoco sé si es más importante la región en la que estoy trabajando o si es mejor irme a distancias más lejanas Cómo haría con la curvatura de la tierra?

--- Proyectos de Astronomía/Laboratorio 1/abrir fits.py
+++ Proyectos de Astronomía/Laboratorio 1/abrir fits.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 ruta = "Proyectos de Astronomía/Laboratorio 1/M41.fits"
 data = fits.getdata(ruta)
-print(data)
+
 print(fits.getval(ruta, "Filter", ext=0))
 print(fits.getval(ruta, "Filter", ext=1))
 print(fits.getval(ruta, "Filter", ext=2))
@@ -19,9 +19,15 @@ plt.hist(data_recortado.ravel(), bins=500, log=True)
 plt.xlabel("Valor del pixel")
 plt.ylabel("Frecuencia")
 plt.title("Histograma recortado (1–99 percentil)")
+plt.savefig("Proyectos de Astronomía/Laboratorio 1/Histograma.png")
 plt.show()
 
 plt.imshow(data, cmap="magma", norm='log')
 plt.colorbar(label="Intensidad (escala log)")
 plt.title("M41")
+plt.savefig(
+    "Proyectos de Astronomía/Laboratorio 1/M41.png",
+    dpi=300,
+    bbox_inches="tight"
+)
 plt.show()
