8ca8da942519f921ec71e3436c2f367f0755f239
Brunelleschk1 <brunelleschki@gmail.com>
2026-02-21 16:38:58 -0500

    Se pueden hacer CMDs, pero no están alineadas las Imágenes y los picos a veces no son muy buenos, siento yo

--- Proyectos de Astronomía/Laboratorio 1/Intento unión.py
+++ Proyectos de Astronomía/Laboratorio 1/Intento unión.py
@@ -1,28 +1,32 @@
-from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
+from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
 from photutils.centroids import centroid_quadratic
+from photutils.aperture import CircularAperture, CircularAnnulus, aperture_photometry
 from skimage.feature import peak_local_max
 from scipy.ndimage import gaussian_filter
+from scipy.spatial import cKDTree
+
 ruta = "Proyectos de Astronomía/Laboratorio 1/M41.fits"
 carpeta1 = "Proyectos de Astronomía/Laboratorio 1/FotosNormales/"
 carpeta2 = "Proyectos de Astronomía/Laboratorio 1/Centroides/"
+carpeta3 = "Proyectos de Astronomía/Laboratorio 1/CentroidesFiltrados/"
 carpeta = "Proyectos de Astronomía/Laboratorio 1/"
+
 Azul = fits.getdata(ruta, ext=0)
 Rojo = fits.getdata(ruta, ext=1)
 Verde = fits.getdata(ruta, ext=2)
 
 def detectar_centroides(imagen, nombre_filtro, carpeta,
-                        min_distance=15,
-                        threshold_factor=10,
+                        min_distance=25,
+                        threshold_factor=15,
                         box_size=7):
 
     mean, median, std = sigma_clipped_stats(imagen, sigma=1.5)
     imagen2 = imagen - median
 
-    # Guardar fondo restado
     plt.figure(figsize=(10,10))
     plt.imshow(imagen2, cmap='magma',vmin=median-std,vmax=median+5*std)
     plt.title(f"{nombre_filtro} fondo restado")
@@ -60,8 +64,9 @@ def detectar_centroides(imagen, nombre_filtro, carpeta,
 
     x_centroids = np.array(x_centroids)
     y_centroids = np.array(y_centroids)
-
-    # Guardar imagen con centroides
+    mask = np.isfinite(x_centroids) & np.isfinite(y_centroids)
+    x_centroids = x_centroids[mask]
+    y_centroids = y_centroids[mask]
     plt.figure(figsize=(10,10))
     plt.imshow(imagen, cmap='magma',vmin=median-std,vmax=median+5*std)
     plt.scatter(x_centroids, y_centroids,s=40, edgecolor='red', facecolor='none')
@@ -69,8 +74,136 @@ def detectar_centroides(imagen, nombre_filtro, carpeta,
     plt.savefig(carpeta2 + f"Centroides_{nombre_filtro}.png",dpi=300, bbox_inches="tight")
     plt.close()
 
+    plt.figure(figsize=(10,10))
+    plt.imshow(imagen2, cmap='magma',vmin=median-std,vmax=median + 5*std)
+    plt.scatter(x_centroids, y_centroids,s=40, edgecolor='cyan', facecolor='none')
+    plt.title(f"Centroides - {nombre_filtro} (Fondo Restado)")
+    plt.savefig(carpeta3 + f"Centroides_{nombre_filtro}_FondoRestado.png",dpi=300, bbox_inches="tight")
+    plt.close()
+
     return x_centroids, y_centroids
 
 x_verde, y_verde = detectar_centroides(Verde, "Verde", carpeta)
 x_azul,  y_azul  = detectar_centroides(Azul,  "Azul",  carpeta)
 x_rojo,  y_rojo  = detectar_centroides(Rojo,  "Rojo",  carpeta)
+
+"""def match_estrellas(x_ref, y_ref, x_mov, y_mov, max_dist=10):
+    coords_mov = np.column_stack((x_mov, y_mov))
+    coords_ref = np.column_stack((x_ref, y_ref))
+    coords_mov = coords_mov[np.all(np.isfinite(coords_mov), axis=1)]
+    coords_ref = coords_ref[np.all(np.isfinite(coords_ref), axis=1)]
+    tree = cKDTree(coords_mov)
+    dist, idx = tree.query(coords_ref, distance_upper_bound=max_dist)
+    mask = dist < max_dist
+    matched_ref = coords_ref[mask]
+    matched_mov = coords_mov[idx[mask]]
+    return matched_ref, matched_mov
+
+def estimar_transformacion(src, dst):
+    N = src.shape[0]
+    A = np.zeros((2*N, 6))
+    b = np.zeros((2*N,))
+    for i in range(N):
+        x, y = src[i]
+        xp, yp = dst[i]
+        A[2*i]   = [x, y, 1, 0, 0, 0]
+        A[2*i+1] = [0, 0, 0, x, y, 1]
+        b[2*i]   = xp
+        b[2*i+1] = yp
+    params, _, _, _ = np.linalg.lstsq(A, b, rcond=None)
+    return params
+
+def aplicar_transformacion(coords, params):
+    a, b, c, d, e, f = params
+    x = coords[:,0]
+    y = coords[:,1]
+    x_new = a*x + b*y + c
+    y_new = d*x + e*y + f
+    return np.column_stack((x_new, y_new))
+
+ref, mov = match_estrellas(x_verde, y_verde,x_azul, y_azul,max_dist=15)
+params_azul = estimar_transformacion(mov, ref)
+coords_azul = np.column_stack((x_azul, y_azul))
+coords_azul_al = aplicar_transformacion(coords_azul, params_azul)
+x_azul_al, y_azul_al = coords_azul_al[:,0], coords_azul_al[:,1]
+
+ref_r, mov_r = match_estrellas(x_verde, y_verde,x_rojo, y_rojo,max_dist=15)
+params_rojo = estimar_transformacion(mov_r, ref_r)
+coords_rojo = np.column_stack((x_rojo, y_rojo))
+coords_rojo_al = aplicar_transformacion(coords_rojo, params_rojo)
+x_rojo_al, y_rojo_al = coords_rojo_al[:,0], coords_rojo_al[:,1]
+
+plt.figure(figsize=(10,10))
+plt.imshow(Verde, cmap='gray', origin='lower')
+plt.scatter(x_verde, y_verde,s=40, edgecolor='lime', facecolor='none', label='Verde')
+plt.scatter(x_azul_al, y_azul_al,s=40, edgecolor='red', facecolor='none', label='Azul alineado')
+plt.scatter(x_rojo_al, y_rojo_al,s=40, edgecolor='cyan', facecolor='none', label='Rojo alineado')
+plt.legend()
+plt.title("Verificación de alineación")
+plt.show()"""
+
+def fotometria_apertura(imagen, x, y,r_ap=5,r_in=7,r_out=10):
+    posiciones = np.transpose((x, y))
+    # Apertura circular
+    aperturas = CircularAperture(posiciones, r=r_ap)
+    # Anillo para fondo local
+    anillos = CircularAnnulus(posiciones, r_in=r_in, r_out=r_out)
+    # Fotometría
+    tabla = aperture_photometry(imagen, aperturas)
+    tabla_anillo = aperture_photometry(imagen, anillos)
+    # Área
+    area_ap = aperturas.area
+    area_an = anillos.area
+    # Fondo medio por píxel
+    fondo_medio = tabla_anillo['aperture_sum'] / area_an
+    # Flujo corregido
+    flujo = tabla['aperture_sum'] - fondo_medio * area_ap
+    return np.array(flujo)
+
+r_ap  = 5      # radio de apertura
+r_in  = 7      # inicio anillo
+r_out = 10     # fin anillo
+
+x_ref = x_azul
+y_ref = y_azul
+
+flujo_verde = fotometria_apertura(Verde, x_ref, y_ref)
+flujo_azul  = fotometria_apertura(Azul,  x_ref, y_ref)
+flujo_rojo  = fotometria_apertura(Rojo,  x_ref, y_ref)
+
+mask = (flujo_verde > 0) & (flujo_azul > 0) & (flujo_rojo > 0)
+
+flujo_verde = flujo_verde[mask]
+flujo_azul  = flujo_azul[mask]
+flujo_rojo  = flujo_rojo[mask]
+
+x_ref = x_ref[mask]
+y_ref = y_ref[mask]
+
+mag_verde = -2.5 * np.log10(flujo_verde)
+mag_azul  = -2.5 * np.log10(flujo_azul)
+mag_rojo  = -2.5 * np.log10(flujo_rojo)
+color_ar = mag_azul - mag_rojo
+color_av = mag_azul - mag_verde
+color_vr = mag_verde - mag_rojo
+
+fig, axs = plt.subplots(1, 3, figsize=(18,6), sharey=True)
+
+axs[0].scatter(color_ar, mag_verde, s=10)
+axs[0].set_xlabel("Azul - Rojo")
+axs[0].set_ylabel("Mag Azul")
+
+axs[1].scatter(color_av, mag_verde, s=10)
+axs[1].set_xlabel("Azul - Verde")
+
+axs[2].scatter(color_vr, mag_verde, s=10)
+axs[2].set_xlabel("Verde - Rojo")
+
+for ax in axs:
+    ax.invert_yaxis()
+    ax.grid(alpha=0.3)
+
+plt.suptitle("Diagramas Color–Magnitud")
+plt.tight_layout()
+plt.savefig(carpeta + "CMD_TresPaneles.png", dpi=300)
+plt.show()
